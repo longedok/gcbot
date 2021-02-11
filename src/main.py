ce82e69ba356ec78f861105a1fd214ddfabb3608
Yauheni Kartavykh <longedok@gmail.com>
2021-02-11 12:39:20 +0300

    Split logging sanitizer into a separate module

--- src/main.py
+++ src/main.py
@@ -3,58 +3,19 @@ from __future__ import annotations
 
 import os
 import logging
-from typing import TYPE_CHECKING, Any, Mapping, Union
-from copy import deepcopy
+from typing import TYPE_CHECKING
 
 from db import init_storage
 from bot import Bot
 from client import Client
 from collector import GarbageCollector
+from privacy import redact_logging_args
 
 if TYPE_CHECKING:
     from logging import LogRecord
 
 
-REDACTED_FIELDS = [
-    "message.from.id",
-    "message.from.first_name",
-    "message.from.last_name",
-    "message.from.username",
-    "message.text",
-]
-
-
-def redact(data: dict, parent: str | None = None) -> None:
-    for k, v in data.items():
-        path = (f"{parent}." if parent else "") + k
-        if isinstance(v, dict):
-            redact(v, path)
-        if path in REDACTED_FIELDS:
-            data[k] = "*" * 3
-
-
-def redacted_copy(data: dict) -> dict:
-    dict_copy = deepcopy(data)
-    redact(dict_copy)
-    return dict_copy
-
-
-LoggingArgs = Union[Mapping[str, Any], tuple]
-
-
 class CustomFormatter(logging.Formatter):
-    def _redact_args(self, args: LoggingArgs) -> LoggingArgs:
-        if isinstance(args, dict):
-            return redacted_copy(args)
-        else:
-            clean_args: list[Any] = []
-            for arg in args:
-                if isinstance(arg, dict):
-                    clean_args.append(redacted_copy(arg))
-                else:
-                    clean_args.append(arg)
-            return tuple(clean_args)
-
     def _shorten_module_name(self, name: str) -> str:
         parts = name.split(".")
         if len(parts) > 1:
@@ -66,7 +27,7 @@ class CustomFormatter(logging.Formatter):
 
     def format(self, record: LogRecord) -> str:
         record.name = self._shorten_module_name(record.name)
-        record.args = self._redact_args(record.args)
+        record.args = redact_logging_args(record.args)
         return super().format(record)
 
 
---
+++ src/privacy.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import Union, Mapping, Any
+from copy import deepcopy
+import re
+
+
+REDACTED_FIELDS = [
+    "message.from.id",
+    "message.from.first_name",
+    "message.from.last_name",
+    "message.from.username",
+    "message.text",
+]
+MASK = "*" * 3
+TOKEN_RE = re.compile(r"/bot([:0-9a-zA-Z]{46})/")
+
+
+def _redact_bot_token(message: str) -> str:
+    if match := TOKEN_RE.search(message):
+        start, end = match.span(1)
+        return message[:start] + MASK + message[end:]
+    return message
+
+
+def _redact(data: dict, parent: str | None = None) -> None:
+    for k, v in data.items():
+        path = (f"{parent}." if parent else "") + k
+        if isinstance(v, dict):
+            _redact(v, path)
+
+        if path in REDACTED_FIELDS:
+            data[k] = MASK
+
+
+def _redacted_copy(data: dict) -> dict:
+    dict_copy = deepcopy(data)
+    _redact(dict_copy)
+    return dict_copy
+
+
+LoggingArgs = Union[Mapping[str, Any], tuple]
+
+
+def redact_logging_args(args: LoggingArgs) -> LoggingArgs:
+    if isinstance(args, dict):
+        return _redacted_copy(args)
+    else:
+        clean_args: list[Any] = []
+        for arg in args:
+            if isinstance(arg, dict):
+                clean_args.append(_redacted_copy(arg))
+            elif isinstance(arg, str):
+                clean_args.append(_redact_bot_token(arg))
+            else:
+                clean_args.append(arg)
+        return tuple(clean_args)
+
