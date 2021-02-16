ff12618c8d8cfd662c5a3b2812a4042dfa1e0f3d
Yauheni Kartavykh <longedok@gmail.com>
2021-02-16 17:22:23 +0300

    Allow basic wildcard in log redaction patterns

--- src/privacy.py
+++ src/privacy.py
@@ -6,11 +6,12 @@ import re
 
 
 REDACTED_FIELDS = [
-    "message.from.id",
-    "message.from.first_name",
-    "message.from.last_name",
-    "message.from.username",
-    "message.text",
+    "*.from.id",
+    "*.from.first_name",
+    "*.from.last_name",
+    "*.from.username",
+    "*.text",
+    "*.chat.title",
 ]
 MASK = "*" * 3
 TOKEN_RE = re.compile(r"/bot([:0-9a-zA-Z_-]{46})/")
@@ -23,18 +24,29 @@ def _redact_bot_token(message: str) -> str:
     return message
 
 
-def _redact(data: dict, parent: str | None = None) -> None:
+def _match_path(path: str, template: str) -> bool:
+    parts = template.split(".")
+    if parts[0] == "*" and len(parts) > 1:
+        ending = ".".join(parts[1:])
+        return path.endswith(ending)
+    else:
+        return path == template
+
+
+def _redact_dict(data: dict, parent: str | None = None) -> None:
     for k, v in data.items():
         path = (f"{parent}." if parent else "") + k
         if isinstance(v, dict):
-            _redact(v, path)
-        if path in REDACTED_FIELDS:
-            data[k] = MASK
+            _redact_dict(v, path)
+        for template in REDACTED_FIELDS:
+            if _match_path(path, template):
+                data[k] = MASK
+                break
 
 
-def _redacted_copy(data: dict) -> dict:
+def _redacted_dict_copy(data: dict) -> dict:
     dict_copy = deepcopy(data)
-    _redact(dict_copy)
+    _redact_dict(dict_copy)
     return dict_copy
 
 
@@ -43,12 +55,12 @@ LoggingArgs = Union[Mapping[str, Any], tuple]
 
 def redact_logging_args(args: LoggingArgs) -> LoggingArgs:
     if isinstance(args, dict):
-        return _redacted_copy(args)
+        return _redacted_dict_copy(args)
     else:
         clean_args: list[Any] = []
         for arg in args:
             if isinstance(arg, dict):
-                clean_args.append(_redacted_copy(arg))
+                clean_args.append(_redacted_dict_copy(arg))
             elif isinstance(arg, str):
                 clean_args.append(_redact_bot_token(arg))
             else:
